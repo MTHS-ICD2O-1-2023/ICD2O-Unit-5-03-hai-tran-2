2eea86751a85fd69370ffe78d1abdaf82cfdd025
Hai Tran <159185497+hai-tran-2@users.noreply.github.com>
2024-04-26 18:44:25 +0000

    add first webpage

--- js/script.js
+++ js/script.js
@@ -1,5 +1,30 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Hai Tran
+// Created on: April 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * this function find movie type based on user age
+ */
+function findMovietype() {
+  // input
+  const userAge = parseFloat(document.getElementById("user-age").value)
+
+  // process
+  if (userAge >= 17) {
+    // output
+    document.getElementById("result").innerHTML = "You can see a R movie alone"
+  } else if (userAge >= 13) {
+    // output
+    document.getElementById("result").innerHTML = "You can see a PG-13 movie alone"
+  } else if (userAge >= 5) {
+    // output
+    document.getElementById("result").innerHTML = "You can see a G ot PG movie alone"
+  } else {
+    // output
+    document.getElementById("result").innerHTML = "You are too young for most movie"
+  }
+}
